dcd9da44f5793e02ceb3d167cbfb14d34905efe7
Gavairon Jordan <117716532+jgavairo@users.noreply.github.com>
2025-08-12 12:02:18 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,132 @@
+const artistsData = {
+    nemes: {
+        id: "nemes",
+        name: "Nemès",
+        tracks: [
+            {
+                title: "Frenesie",
+                spotify: "https://open.spotify.com/intl-fr/track/6r5vIy222xPe3CnN2D386g",
+                youtube: "https://www.youtube.com/watch?v=Sm6Jib4_ZLE",
+                image: "assets/images/tracks/artist_nemes.png"
+            },
+            {
+                title: "433",
+                spotify: "https://open.spotify.com/intl-fr/track/5VW3cH9hl8Y6WU7GSnlaYU",
+                youtube: "https://www.youtube.com/watch?v=YQk8x75_O8U",
+                image: "assets/images/tracks/artist_nemes.png"
+            },
+            {
+                title: "Le poids du monde",
+                spotify: "https://open.spotify.com/track/41pywQ3mS3pXKCQzosi24T",
+                youtube: "https://www.youtube.com/watch?v=_gv8S70JI_o",
+                image: "assets/images/tracks/lpdm_nemes.png"
+            },
+            {
+                title: "Océan",
+                spotify: "https://open.spotify.com/intl-fr/track/3BZebGwOIp2nS7m3yVthmk",
+                youtube: "https://www.youtube.com/watch?v=WY0ipuk3I08",
+                image: "assets/images/tracks/ocean.png"
+            },
+            {
+                title: "Emotion double",
+                spotify: "https://open.spotify.com/intl-fr/track/0xxYZ5ZJEEg8yR8E9KBi78",
+                youtube: "https://www.youtube.com/watch?v=fvbribWRRBM",
+                image: "assets/images/tracks/emotion_double.png"
+            },
+            {
+                title: "Plus là",
+                spotify: "https://open.spotify.com/intl-fr/track/3KePlyK0GW3LIfb9mQLiXy",
+                image: "assets/images/tracks/tous_les_jours.png"
+            },
+            {
+                title: "Tous les jours",
+                spotify: "https://open.spotify.com/intl-fr/track/1UWviQWLW7YZLCrQs1hGmO",
+                youtube: "https://www.youtube.com/watch?v=8Uqc6bCivDI",
+                image: "assets/images/tracks/plus_la.png"
+            }
+        ]
+    },
+    louv: {
+        id: "louv",
+        name: "Louv",
+        tracks: [
+            {
+                title: "Elle sait pas",
+                spotify: "https://open.spotify.com/intl-fr/track/3oPzchMhkOciIuwt1R7rJM",
+                image: "assets/images/artist_louv.png"
+            },
+            {
+                title: "Immortel",
+                spotify: "https://open.spotify.com/intl-fr/track/1drvEs2HIQgDABBxo8N9Z9",
+                image: "assets/images/artist_louv.png"
+            }
+        ]
+    },
+    cabrino: {
+        id: "cabrino",
+        name: "Cabrino",
+        tracks: [
+            {
+                title: "Inimaginable",
+                spotify: "https://open.spotify.com/intl-fr/track/0Pl0iRG4lUN3s4Z8qEITOI",
+                image: "assets/images/artist_cabrino.png"
+            },
+            {
+                title: "Lumière",
+                spotify: "https://open.spotify.com/intl-fr/track/3me1JibGEerGeWhBYutpM7", 
+                image: "assets/images/artist_cabrino.png"
+            },
+            {
+                title: "Shawty",
+                spotify: "https://open.spotify.com/intl-fr/track/1p1dKQGMbvHka4yDJTL1gZ",
+                image: "assets/images/artist_cabrino.png"
+            },
+            {
+                title: "Sable, mer, coquillages",
+                spotify: "https://open.spotify.com/intl-fr/track/5MMTnRt9H0mqLQCMrKw6iX",
+                image: "assets/images/tracks/sablemercoquillage.png"
+            },
+            {
+                title: "Flamme",
+                spotify: "https://open.spotify.com/intl-fr/track/4D3H4LOX4T7en9K5YLz4yF",
+                image: "assets/images/tracks/makima.png"
+            },
+            {
+                title: "Le doute",
+                spotify: "https://open.spotify.com/intl-fr/track/3Alz4hCTCVJN6cIPN9U1qt",
+                image: "assets/images/tracks/makima.png"
+            },
+            {
+                title: "Vers le vide",
+                spotify: "https://open.spotify.com/intl-fr/track/6WVytflOnmBvBC069EII7c",
+                image: "assets/images/tracks/makima.png"
+            },
+            {
+                title: "Avant tout le reste",
+                spotify: "https://open.spotify.com/intl-fr/track/6WVytflOnmBvBC069EII7c",
+                image: "assets/images/tracks/makima.png"
+            },
+            {
+                title: "Soleil",
+                spotify: "https://open.spotify.com/intl-fr/track/1CjJqJBkufYVNzMCfKiFr5",
+                image: "assets/images/tracks/makima.png"
+            },
+            {
+                title: "Wow",
+                spotify: "https://open.spotify.com/intl-fr/track/4XpP430mySrUcVDSUchAcW",
+                image: "assets/images/tracks/makima.png"
+            }
+        ]
+    },
+    jass: {
+        id: "jass",
+        name: "Jass",
+        tracks: [
+            {
+                title: "Ça arrive bientôt..",
+                image: "assets/images/artist_jass.png",
+                spotify: "#"
+            }
+        ]
+    }
+}; 
